2d07ed203267f8ab1638cb6c036ca96288d006cd
MadsDamiri87 <madsc.damiri@gmail.com>
2026-09-04 21:42:59 +0200

    Repository added

---
+++ Server/Entities/Comment.cs
@@ -0,0 +1,15 @@
+﻿namespace Entities;
+
+public class Comment
+{
+    public int Id {get; set;}
+    public int PostId {get; set;}
+    public int User {get; set;}
+    
+    public string Body {get; set;}
+    public DateTime Date {get; set;}
+    
+    
+    
+    
+}
---
+++ Server/Entities/SubForum.cs
@@ -0,0 +1,10 @@
+﻿namespace Entities;
+
+public class SubForum
+{
+    public int Id {get; set;}
+    public string SubForumName {get; set;}
+    public int CreatorId {get; set;}
+    public DateTime DateCreated {get; set;}
+    
+}
