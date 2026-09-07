092b421f6bba81c63ac2228ca28133a38f8fc5a4
MadsDamiri87 <madsc.damiri@gmail.com>
2026-09-07 16:02:26 +0200

    Changed: init instead of 'set' for properties that should not change after initialization

--- Server/Entities/Post.cs
+++ Server/Entities/Post.cs
@@ -2,7 +2,7 @@
 
 public class Post : IEntity
 {
-    public int Id {get; set;}
+    public int Id {get; init;}
     public int SubForumId {get; set;}
     public int UserId {get; set;}
     
--- Server/Entities/User.cs
+++ Server/Entities/User.cs
@@ -2,7 +2,7 @@
 
 public class User : IEntity
 {
-    public int Id {get; set;}
+    public int Id {get; init;}
     public required string UserName {get; set;}
     public required string PasswordHash {get; set;}
     public required string Email {get; set;}
