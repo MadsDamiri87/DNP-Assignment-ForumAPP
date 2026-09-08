017c6872d2ce89719691b6e39f96bf4d51aad284
MadsDamiri87 <madsc.damiri@gmail.com>
2026-09-08 09:16:02 +0200

    constructor added to CliApp.cs, and CLI module added to project

---
+++ Server/CLI/UI/CliApp.cs
@@ -0,0 +1,25 @@
+﻿using RepositoryContracts;
+
+namespace CLI.UI;
+
+public class CliApp
+{
+    private readonly IUserRepository userRepository;
+    private readonly ICommentRepository commentRepository;
+    private readonly IPostRepository postRepository;
+
+    public CliApp(
+        IUserRepository userRepository, 
+        ICommentRepository commentRepository, 
+        IPostRepository postRepository)
+    {
+        this.userRepository = userRepository;
+        this.commentRepository = commentRepository;
+        this.postRepository = postRepository;
+    }
+
+    public async Task StartAsync()
+    {
+        
+    }
+}
